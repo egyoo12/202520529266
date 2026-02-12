3df408ad7c195e00972f685171ba016de7e7001e
Oluwatamilore Osuntubo <egyoo12@nottingham.ac.uk>
2026-02-12 02:43:35 +0000

    add calc.cpp example program

---
+++ worksheet3/adder.h
@@ -0,0 +1,13 @@
+//begin - adder.h
+
+//the first two lines of code here prevent adder.h from being 
+//included more than once in any .cpp source file.
+
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+//prototype for our function
+int add (int a, int b);
+
+#endif
+//end - adder.h
