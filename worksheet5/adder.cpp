1dc13706b1dd3a704b92d8b3858b2b5f1e4ae163
Oluwatamilore Osuntubo <egyoo12@nottingham.ac.uk>
2026-02-12 06:13:32 +0000

    exercises for worksheet 5

---
+++ worksheet5/adder.cpp
@@ -0,0 +1,10 @@
+//begin - adder.cpp
+
+#include "adder.h"
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+//end - adder.cpp
---
+++ worksheet5/adder.h
@@ -0,0 +1,20 @@
+//begin - adder.h
+
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+/** @file
+* this file contains the declarations of all exported functions in the maths library.
+*/
+
+/**adding function
+* this function adds two numbers
+* @param a is the first number
+* @param b is the second number
+* @return sum of a and b
+*/
+
+int add (int a, int b);
+
+#endif
+//end - adder.h
