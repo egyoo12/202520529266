ad34d41543d9af08100108cceecc31c511bc533e
Oluwatamilore Osuntubo <egyoo12@nottingham.ac.uk>
2026-03-03 02:58:55 +0000

    worksheet 6, exercise 1: created qt project and basic GUI layout

---
+++ worksheet6/exercise1_build/.qt/QtDeployTargets-RelWithDebInfo.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_exercise1_FILE C:/Users/osunt/202520529266/worksheet6/exercise1_build/RelWithDebInfo/exercise1.exe)
+set(__QT_DEPLOY_TARGET_exercise1_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_exercise1_RUNTIME_DLLS C:/Qt/6.10.2/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Core.dll)
