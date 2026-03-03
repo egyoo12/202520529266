c55d08edf6b8e4cdd3942ceab192ad99fae94d56
Oluwatamilore Osuntubo <egyoo12@nottingham.ac.uk>
2026-03-03 03:15:01 +0000

    reuploading worksheet6 folder

--- worksheet6/exercise1_build/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_exercise1_FILE C:/Users/osunt/202520529266/worksheet6/exercise1_build/Debug/exercise1.exe)
-set(__QT_DEPLOY_TARGET_exercise1_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_exercise1_RUNTIME_DLLS C:/Qt/6.10.2/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Cored.dll)
